f9837e34acfe5cb1edae64d615ff1cb207f56948
emilarar5v3 <118186445+emilarar5v3@users.noreply.github.com>
2022-11-28 16:35:48 +0600

    Initial commit

---
+++ src/Contact.java
@@ -0,0 +1,102 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Contact {
+
+
+ private    String    name  ;
+    private int   cod  ;
+ private int   number  ;
+
+    public Contact(String name, int cod, int number) {
+        this.name = name;
+        this.cod = cod;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    void number (){
+
+        System.out.println("выберите  контактов    aleks   0700344322 ,igor 0705234243, atabek 0707345434, aman 0555342342 " );
+        Scanner sc = new Scanner(System.in);
+        System.out.println("выберите номер ");
+        number = sc.nextInt();
+        switch (number){
+            case 1 :
+                System.out.println(" igor  0705234243");
+                break;
+            case 2:
+                System.out.println("atabek 0707345434");
+                break;
+            case 3:
+                System.out.println("aman 0555342342");
+                break;
+            default:
+                System.out.println(number);
+
+        }
+    }
+  void enter (){
+      System.out.println(" доступные коды 0700 ,  0550 ,  0774 вы можете выбрать одно из них");
+      System.out.println( " ведите код который хотите сохранить");
+      Scanner scanner = new Scanner(System.in);
+      cod = scanner.nextInt();
+      switch (cod){
+          case 1:
+              System.out.println(700);
+          break;
+          case  2:
+              System.out.println(550);
+              break;
+          case 3:
+               System.out.println(774);
+
+          default:
+              System.out.println(cod);
+      }
+      System.out.println("ведите номер сох");
+      number = scanner.nextInt();
+
+  }
+  void  name(){
+      System.out.println("ведите имя контакт ");
+      Scanner sc = new Scanner(System.in);
+      name = sc.nextLine();
+      System.out.println( " ваш контакт успешно сохранен "+ name + "  " + number);
+  }
+
+
+
+
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                " имя контактов ='" +this. name + '\'' +
+                ", cod который вы должны дать =" + this.cod +
+                ",номер телефона number=" +this.number +
+                '}';
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args){
+        Contact pd = new Contact("name " , 999, 770855442);
+ Contact [] contacts = new Contact[4];
+ contacts [0] = new Contact("aleks ", 707, 73333333);
+contacts [1]= new Contact("igor " , 556, 555653465);
+contacts [2] = new Contact("aman " , 704, 704456723);
+contacts[3] = new Contact("andrei" , 7777, 777765432);
+for (int i = 0;  i< contacts.length; i++){
+    System.out.println(contacts[i]);
+}
+System.out.println(pd);
+pd.number();
+
+pd.enter();
+        pd.name();
+
+
+
+
+
+
+
+
+
+
+ }
+
+        }
+
+
